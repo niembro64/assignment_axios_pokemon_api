a8c3fa043527d83b99b2202a161e97d60d95dccf
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-19 02:11:18 -0400

    logging funct

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
 import "./App.css";
 
 // fetch all 807 pokemon with a button from https://pokeapi.co
 
 function App() {
+  const onClickHandler = () => {
+    console.log("clicked eric");
+  };
+
   return (
     <div className="App">
       <h1>test eric</h1>
+      <button className="btn btn-primary mx-4" onClick={onClickHandler}>
+        Fetch Pokemon
+      </button>
     </div>
   );
 }
